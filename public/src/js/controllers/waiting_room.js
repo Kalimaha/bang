59c8a196967c39da8be398c69fc0c8fe8fde85c1
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-01-12 15:45:30 +1100

    Join existing game.

--- public/src/js/controllers/waiting_room.js
+++ public/src/js/controllers/waiting_room.js
@@ -4,11 +4,37 @@ app.controller('WaitingRoomController', function($scope, $routeParams, $http, $c
   $scope.create_game = () => {
     const game_id = create_game_id()
 
-    firebase.database().ref(game_id).once('value').then((snapshot) => {
+    firebase.database().ref(`games/${game_id}`).once('value').then((snapshot) => {
       if (snapshot.val() === null)  { create_game(game_id) }
       else                          { $scope.create_game() }
     })
   }
+
+  $scope.join_game = () => {
+    const game_id = $('#existing_game_id').val()
+
+    if (game_id === undefined || game_id.length < 4) {
+      log('The Game ID must have 4 digits, e.g. 1234.')
+    } else {
+      firebase.database().ref(`games/${game_id}`).once('value').then((snapshot) => {
+        $('#error_message').css('display', 'none')
+
+        if (snapshot.val() === null) { log(`There is no Game available with ID "${game_id}".`) }
+        else {
+          const new_player = game_player()
+
+          firebase.database()
+          .ref(`games/${game_id}/players/${$cookieStore.get('user').id}`)
+          .update(new_player)
+          .then(() => {
+            window.location = `#!/games/${game_id}`
+          })
+        }
+      })
+    }
+  }
+
+  const log = (error) => $('#error_message').css('display', 'block').html(error)
 
   const create_game = (game_id) => {
     firebase.database()
